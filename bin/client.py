9d71422082e17bd3a628a02a7a2e7f65fd6ca9c6
rkouere <rkouere@gmail.com>
2015-10-11 15:02:29 +0200

    c'est plus propre comme ça

---
+++ bin/client.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python3
+
+
+def main():
+    print("coucou, je suis client")
+
+
+# This is a Python's special:
+# The only way to tell wether we are running the program as a binary,
+# or if it was imported as a module is to check the content of
+# __name__.
+# If it is `__main__`, then we are running the program
+# standalone, and we run the main() function.
+if __name__ == "__main__":
+    main()
