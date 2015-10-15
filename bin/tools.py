bc0ad1c8b9869bdee61027745895b7987a90d4b2
rkouere <rkouere@gmail.com>
2015-10-15 19:37:42 +0200

    un peut de netoyage + commencement de l'implem d'une fonction permettant de créer un packet http

--- bin/tools.py
+++ bin/tools.py
@@ -1,9 +1,55 @@
 #! /usr/bin/env python3
 
+hardReturn = "\r\n"
+
+
+class httpPacket:
+    def __init__(self):
+        """
+        Set the typical header
+        """
+        self.header = (
+            "HTTP/1.1 200 OK" + hardReturn
+            + "Content-type: application/octet-stream" + hardReturn
+            + "Transfer-Encoding: chunked" + hardReturn)
+        self.data = ""
+        self.contentLength = ""
+
+    def setCookie(self, token):
+        """
+        Sets a cookie. Has to be a digit
+        """
+        self.header += "Set-Cookie: tok=" + str(token) + hardReturn
+
+    def setData(self, data):
+        """
+        Sets the data to send
+        Sets the length of the data
+        """
+        self.data = data
+        self.setContentLength()
+
+    def setContentLength(self):
+        self.contentLength = 
+            "Content-length: " + str(len(self.data)) + hardReturn
+
+    def getPacket(self):
+        """
+        Constructs the http packet to send
+        """
+        packet = self.header + self.contentLength + self.data
+        return packet
+
+
+def test():
+    print("test")
+
 
 def main():
-    print("coucou")
-
+    packet = httpPacket()
+    packet.setCookie(1234)
+    packet.setData("salut tout le monde")
+    print(packet.getPacket())
 
 # This is a Python's special:
 # The only way to tell wether we are running the program as a binary,
