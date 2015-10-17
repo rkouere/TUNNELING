bc70d2162dbeaebb9959b6309604cc2808891fd6
rkouere <rkouere@vps205524.ovh.net>
2015-10-17 07:50:49 +0200

    serveur accept connection

--- bin/server.py
+++ bin/server.py
@@ -1,28 +1,94 @@
-import socketserver
+import socket
+import threading
 
-class MyTCPHandler(socketserver.BaseRequestHandler):
-    """
-    The RequestHandler class for our server.
+class myThread (threading.Thread):
+    def __init__(self, socket, address):
+        self.socket = socket
+        self.address = address[0]
+        self.port = address[1]
+        print("connection accepted with " + self.address)
+    def run(self):
+        self.socket.sendall("coucou".encode())
 
-    It is instantiated once per connection to the server, and must
-    override the handle() method to implement communication to the
-    client.
-    """
 
-    def handle(self):
-        # self.request is the TCP socket connected to the client
-        self.data = self.request.recv(1024).strip()
-        print("{} wrote:".format(self.client_address[0]))
-        print(self.data)
-        # just send back the same data, but upper-cased
-        self.request.sendall(self.data.upper())
+
+class server:
+    def __init__(self, host, port):
+        # create an INET, STREAMing socket
+        self.serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # bind the socket to a public host, and a well-known port
+        self.serversocket.bind((host, port))
+        # become a server socket
+        self.serversocket.listen(5)
+    
+    def accept(self):
+        while True:
+            # accept connections from outside
+            (clientsocket, address) = self.serversocket.accept()
+            # now do something with the clientsocket
+            # in this case, we'll pretend this is a threaded server
+            ct = myThread(clientsocket, address)
+            ct.run()
+
 
 if __name__ == "__main__":
-    HOST, PORT = "",80 
+    HOST = ""
+    PORT = 80
+    serv = server(HOST, PORT)
+    serv.accept()
 
-    # Create the server, binding to localhost on port 9999
+
+
+
+
+
+
+
+
+"""
+import socketserver, subprocess, sys
+from threading import Thread
+import time
+
+
+def handle(self):
+    # self.request is the client connection
+    data = self.request.recv(1024)  # clip input at 1Kb
+    self.request.send(reply)
+    self.request.close()
+
+
+class SingleTCPHandler(socketserver.BaseRequestHandler):
+    "One instance per connection.  Override handle(self) to customize action."
+    def handle(self):
+        while True:
+            # self.request is the client connection
+            data = self.request.recv(1024)  # clip input at 1Kb
+            print("received " + data.decode())
+            self.request.send("coucou".encode())
+            self.request.close()
+            time.sleep(0.1)
+
+class SimpleServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
+    # Ctrl-C will cleanly kill all spawned threads
+    daemon_threads = True
+    # much faster rebinding
+    allow_reuse_address = True
+
+    def __init__(self, server_address, RequestHandlerClass):
+        socketserver.TCPServer.__init__(self, server_address, RequestHandlerClass)
+
+
+
+
+if __name__ == "__main__":
+    HOST = ""
+    PORT = 80
+    server = SimpleServer((HOST, PORT), SingleTCPHandler)
+    # terminate with Ctrl-C
+    try:
+        server.serve_forever()
+    except KeyboardInterrupt:
+        sys.exit(0)    # Create the server, binding to localhost on port 9999
     server = socketserver.TCPServer((HOST, PORT), MyTCPHandler)
-
-    # Activate the server; this will keep running until you
-    # interrupt the program with Ctrl-C
-    server.serve_forever()
+"""
