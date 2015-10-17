35f8c535cda112da8816b2dec7eab8111fbe12a1
rkouere <rkouere@gmail.com>
2015-10-17 18:58:51 +0200

    ajout methode pour recuper seulement la data

--- bin/tools.py
+++ bin/tools.py
@@ -74,128 +74,6 @@ class sshToHttp(threading.Thread):
             else:
                 break
         self.ssh.close()
-"""
-SERVER
-"""
-
-
-class myThread (threading.Thread):
-    def __init__(self, socket, address, conn):
-        """
-        Initialise the variables needed to communicate
-        """
-        self.socket = socket
-        self.address = address[0]
-        self.port = address[1]
-        self.conn = conn
-        print("connection accepted with " + self.address)
-
-    def log(self, message):
-        print("[server] " + message)
-
-    def addConn(self, con):
-        self.con = con
-
-    def run(self):
-        if self.conn is False:
-            self.communicate()
-        else:
-            self.sshCommunicate()
-
-    def sshCommunicate(self):
-        """
-        Receives a connection on the port listening to TCP connections
-        Sends the data ssh
-        """
-        while True:
-            data = self.receive()
-            self.log("received " + data.decode())
-            if data is False:
-                break
-            self.conn.send(data)
-
-    def receive(self):
-        """
-        Try to receive a connection.
-        Return False if there is the connection has to be closed
-        (if it receives no data)
-        """
-        data = self.socket.recv(1024)
-        if not data:
-            return False
-        else:
-            self.log("received data : " + data.decode())
-            return data
-
-    def send(self, data):
-        """
-        Sends a data string
-        If the connection is closed, close the connection
-        """
-        self.socket.sendall(data.encode())
-        self.log("sent " + str(data))
-        time.sleep(0.1)
-
-    def communicate(self):
-        """
-        Sends the reply
-        Waits for other connections and sends the reply
-        If the connection is closed, stops the loop
-        """
-        tok = 1
-        self.socket.sendall(str(tok).encode())
-        while True:
-            tok += 1
-            data = self.receive()
-            if data is False:
-                break
-            self.send(str(tok))
-
-
-class server:
-    def __init__(self, host, port):
-        # create an INET, STREAMing socket
-        self.serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.host = host
-        self.port = port
-
-    def acceptHTTP(self, conn):
-        """
-        Waits for a connection on the socket
-        When a connection has been made, starts a new thread
-        """
-        # bind the socket to a public host, and a well-known port
-        self.serversocket.bind((self.host, self.port))
-        # become a server socket
-        self.serversocket.listen(5)
-        while True:
-            # accept connections from outside
-            (clientsocket, address) = self.serversocket.accept()
-            # now do something with the clientsocket
-            # in this case, we'll pretend this is a threaded server
-
-            try:
-                ct = myThread(clientsocket, address, conn)
-                ct.run()
-            except KeyboardInterrupt:
-                print("socket broken. Closing the connection")
-                clientsocket.close()
-
-    def connectToSSH(self, conn):
-        try:
-            self.remote_ip = socket.gethostbyname(self.host)
-            print("Host " + self.host + " is on ip " + self.remote_ip)
-        except socket.gaierror:
-            # could not resolve
-            self.log('Hostname could not be resolved. Exiting')
-            sys.exit()
-            try:
-                ct = myThread(self.serversocket, 1, conn)
-                ct.run()
-            except KeyboardInterrupt:
-                print("socket broken. Closing the connection")
-                self.clientsocket.close()
-
 
 """
 CLIENT
@@ -291,7 +169,7 @@ class httpPacket:
             + "Transfer-Encoding: chunked" + hardReturn
             + "Proxy-Connection: ")
         self.GETHeader = (
-            "GET /cgi-bin/test.py"
+            "GET /"
             + " HTTP/1.1" + hardReturn
             + "Connection: keep-alive" + hardReturn
             + "Cache-Control: no-cache" + hardReturn)
@@ -319,11 +197,19 @@ class httpPacket:
         self.contentLength = "Content-length: " + str(
             len(self.data)) + hardReturn
 
+    def getData(self, httpPacket):
+        """
+        Returns the data of a http packet
+        """
+        data_string = str(httpPacket)
+        data_index = data_string.find(hardReturn + hardReturn)
+        return data_string[data_index:]
+
     def getGETPacket(self):
         """
         Constructs the http packet to send
         """
-        packet = self.GETHeader + self.host + hardReturn
+        packet = self.GETHeader + self.host + hardReturn + self.data
         return packet
 
     def getOkPacket(self):
@@ -336,10 +222,12 @@ def test():
 
 
 def main():
-    packet = httpPacket()
+    packet = httpPacket("localhost")
     packet.setCookie(1234)
     packet.setData("salut tout le monde")
     print(packet.getGETPacket())
+    print("====================")
+    print(packet.getData(packet.getGETPacket()))
 
 # This is a Python's special:
 # The only way to tell wether we are running the program as a binary,
