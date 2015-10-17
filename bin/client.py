cf0de25af91257b32190aa4bd63a710481455af1
rkouere <rkouere@gmail.com>
2015-10-17 15:44:17 +0200

    shell ok

--- bin/client.py
+++ bin/client.py
@@ -1,13 +1,37 @@
 #! /usr/bin/env python3
 import sys
-from tools import client, httpPacket
-
+from tools import client, httpPacket, sshToHttp
+import socket
+import time
 
 def main(host, port):
-    # connection a la machine distante
-    http_con = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    http_con.connect((host, port))
-    
+    """
+    Attend une connection sur le port port
+
+    """
+    ssh_con = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    ssh_con.bind(("", port))
+    # become a server socket
+    ssh_con.listen(5)
+    # accept connections from outside
+    (ssh_socket, address) = ssh_con.accept()
+    print("connection ssh created")
+    data = ssh_socket.recv(1024)
+    print("ssh data = " + data.decode())
+    http_con = client(host, 80)
+    sshToHttp(ssh_socket, http_con, data).start()
+    while True:
+        print("[http] waiting for data")
+        data = ssh_socket.recv(1024)
+        if data:
+            print("[http] data " + str(data))
+            print("[http] sending the data")
+            http_con.send(data)
+            time.sleep(0.1)
+        else:
+            break
+    http_con.close()
+
 
 # This is a Python's special:
 # The only way to tell wether we are running the program as a binary,
