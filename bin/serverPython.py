739b5ac40546b24039e002c9e6ce56adb3e767e6
rkouere <rkouere@gmail.com>
2015-10-17 16:20:58 +0200

    un peut de netoyage

--- bin/serverPython.py
+++
@@ -1,6 +0,0 @@
-from http.server import CGIHTTPRequestHandler, HTTPServer
-
-handler = CGIHTTPRequestHandler
-handler.cgi_directories = ['/cgi-bin', '/htbin']  # this is the default
-server = HTTPServer(('localhost', 80), handler)
-server.serve_forever()
