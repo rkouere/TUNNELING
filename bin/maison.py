1f1f7b891f327ae5b31da4abcf607444c92cc22e
rkouere <rkouere@gmail.com>
2015-10-17 16:06:40 +0200

    refactorisation

--- bin/maison.py
+++ bin/maison.py
@@ -1,41 +1,8 @@
 import socket
 import sys
 import time
-from tools import client, sshToHttp
-
-
-def sshListening(host, port):
-    """
-    Listen to a tcp connection on port XX
-    When a connection has been made
-        - creates a new thread
-        - creates a new socket and connects to the ssh server
-        - redirects the connections made by the ssh server to the http socket
-    """
-    http_con = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    http_con.bind(("", port))
-    # become a server socket
-    http_con.listen(5)
-    # accept connections from outside
-    (http_socket, address) = http_con.accept()
-    print("connection tcp created")
-    data = http_socket.recv(1024)
-    print("http data = " + data.decode())
-    ssh_con = client("localhost", 22)
-    sshToHttp(http_socket, ssh_con, data).start()
-    # on continue de recevoir ce que http recoit et de le renvoyer à ssh
-    while True:
-        print("[http] waiting for data")
-        data = http_socket.recv(1024)
-        if data:
-            print("[http] data " + str(data))
-            print("[http] sending the data")
-            ssh_con.send(data)
-            time.sleep(0.1)
-        else:
-            break
-    ssh_con.close()
+from tools import client, tunnel
 
 
 if __name__ == "__main__":
-    sshListening(sys.argv[1], int(sys.argv[2]))
+    tunnel(sys.argv[1], int(sys.argv[2]), int(sys.argv[3])).init()
