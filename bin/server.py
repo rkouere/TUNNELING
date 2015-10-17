71772af1d300d546ffe973145d60dd2ec89af903
rkouere <rkouere@vps205524.ovh.net>
2015-10-17 06:51:34 +0200

    utilisation de socketserver et connection ok

--- bin/server.py
+++ bin/server.py
@@ -1,60 +1,28 @@
-#! /usr/bin/env python3
-import socket
-import sys
-from tools import httpPacket, server
+import socketserver
 
+class MyTCPHandler(socketserver.BaseRequestHandler):
+    """
+    The RequestHandler class for our server.
 
+    It is instantiated once per connection to the server, and must
+    override the handle() method to implement communication to the
+    client.
+    """
 
+    def handle(self):
+        # self.request is the TCP socket connected to the client
+        self.data = self.request.recv(1024).strip()
+        print("{} wrote:".format(self.client_address[0]))
+        print(self.data)
+        # just send back the same data, but upper-cased
+        self.request.sendall(self.data.upper())
 
-def clientthread(conn):
-    # Sending message to connected client
-    # send only takes string
-    conn.send(b'Welcome to the server. Type something and hit enter\n')
-    # infinite loop so that function do not terminate and thread do not end.
-    while True:
-        # Receiving from client
-        data = conn.recv(1024)
-        reply = 'OK...' + data.decode()
-        if not data:
-            break
-        conn.sendall(reply.encode())
-    # came out of loop
-    conn.close()
-
-
-def main(host, port):
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    print('Socket created')
-    # Bind socket to local host and port
-    try:
-        s.bind((host, port))
-    except socket.error as msg:
-        print('Bind failed. Error Code : ' +
-              str(msg[0]) + ' Message ' + msg[1])
-        sys.exit()
-    print('Socket bind complete')
-    # Start listening on socket
-    s.listen(10)
-    print('Socket now listening')
-    # Function for handling connections. This will be used to create threads
-    # now keep talking with the client
-    while 1:
-        # wait to accept a connection - blocking call
-        conn, addr = s.accept()
-        print('Connected with ' + addr[0] + ':' + str(addr[1]))
-        # start new thread takes 1st argument as a function name to be run,
-        # second is the tuple of arguments to the function.
-        clientthread(conn)
-    s.close()
-
-
-# This is a Python's special:
-# The only way to tell wether we are running the program as a binary,
-# or if it was imported as a module is to check the content of
-# __name__.
-# If it is `__main__`, then we are running the program
-# standalone, and we run the main() function.
 if __name__ == "__main__":
-    main(sys.argv[1], int(sys.argv[2]))
+    HOST, PORT = "",80 
 
+    # Create the server, binding to localhost on port 9999
+    server = socketserver.TCPServer((HOST, PORT), MyTCPHandler)
 
+    # Activate the server; this will keep running until you
+    # interrupt the program with Ctrl-C
+    server.serve_forever()
