453af8e49132444334bc1b5ec3810901525b01ae
rkouere <rkouere@gmail.com>
2015-10-24 11:27:14 +0200

    cleaning up

--- bin/maison.py
+++ bin/maison.py
@@ -4,29 +4,20 @@ Ecoute sur le port 80 les connections qui viennent à elle
 Envoie les packet du port 80 au ssh
 Envoie les packet du ssh vers le port 80
 """
-from http.server import  BaseHTTPRequestHandler, HTTPServer
+from http.server import BaseHTTPRequestHandler, HTTPServer
 import sys
-import time
 import socket
-from tools import client, httpPacket, processHttpRequests, proceesSSHRequests
-import threading
-import time
-from io import StringIO
 import cgi
-from urllib.parse import parse_qs
 import base64 as B64
 
+
 class MyServer(BaseHTTPRequestHandler):
-    
-    def __init__(self,ssh_socket,*args):
-        self.ssh=ssh_socket
-        BaseHTTPRequestHandler.__init__(self,*args)
+    def __init__(self, ssh_socket, *args):
+        self.ssh = ssh_socket
+        BaseHTTPRequestHandler.__init__(self, *args)
 
     def do_GET(self):
         self.send_response(200)
-        
-        
-	
         self.ssh.settimeout(0.1)
         try:
             data = self.ssh.recv(4092)
@@ -35,41 +26,44 @@ class MyServer(BaseHTTPRequestHandler):
             self.end_headers()
             self.wfile.write(B64.b64encode(data))
         except socket.timeout:
-            print( "No response from server")
+            print("No response from server")
             self.send_header("Content-type", "text/html")
             self.end_headers()
-            self.wfile.write(bytes("<html><head><title>GET : Title goes here.</title></head><body>TEST</body></html>", "utf-8"))
-        
+            self.wfile.write(bytes(
+                "<html><head><title>GET : Title goes here.</title>" +
+                "</head><body>TEST</body></html>", "utf-8"))
 
     def do_POST(self):
-       	if(self.headers.get('Content-Type',False) ): 
+        if(self.headers.get('Content-Type', False)):
             ctype, pdict = cgi.parse_header(self.headers['Content-Type'])
             print(str(ctype))
             if ctype == 'application/x-www-form-urlencoded':
-              length = int(self.headers['Content-Length'])
-              print(str(length))
-              data= B64.b64decode(self.rfile.read(length))
+                length = int(self.headers['Content-Length'])
+                print(str(length))
+                data = B64.b64decode(self.rfile.read(length))
             else:
-              print("parse error")
-        
-            print(str(data))
-            #data=postvars['data'.encode()][0]
-            if data :
-               self.ssh.sendall(data)
-       
-        self.send_response(200)
-        
-        #self.send_header("Content-type", "text/html")
-        self.end_headers()
-        #self.wfile.write()
-"""
-        self.wfile.write(bytes("<html><head><title>POST :Title goes here.</title></head>", "utf-8"))
-        self.wfile.write(bytes("<body><p>This is a test.</p>", "utf-8"))
-        self.wfile.write(bytes("<p>You accessed path: %s</p>" % self.path, "utf-8"))
-        self.wfile.write(bytes("</body></html>", "utf-8"))
-"""
+                print("parse error")
 
-	
+            print(str(data))
+            # data=postvars['data'.encode()][0]
+            if data:
+                self.ssh.sendall(data)
+
+        self.send_response(200)
+
+        # self.send_header("Content-type", "text/html")
+        self.end_headers()
+        # self.wfile.write()
+        """
+        self.wfile.write(bytes(
+            "<html><head><title>POST :Title goes here.</title>" +
+            "</head>", "utf-8"))
+        self.wfile.write(bytes("<body><p>This is a test.</p>", "utf-8"))
+        self.wfile.write(bytes(
+        "<p>You accessed path: %s</p>" % self.path, "utf-8"))
+        self.wfile.write(bytes("</body></html>", "utf-8"))
+        """
+
 
 class tunnel:
     """
@@ -82,9 +76,7 @@ class tunnel:
         self.ssh_port = ssh_port
 
     def init(self):
-        outFile=StringIO() 
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        
         # Bind socket to local host and port
         try:
             sock.bind((self.host, self.ssh_port))
@@ -97,33 +89,33 @@ class tunnel:
         sock.listen(10)
 
         print('Socket now listening')
-        # Function for handling connections. This will be used to create threads
+        # Function for handling connections.
+        # This will be used to create threads
         # now keep talking with the client
-        #while 1:
-         
+        # while 1:
         # wait to accept a connection - blocking call
         conn, addr = sock.accept()
-        
 
         print('Connected with ' + addr[0] + ':' + str(addr[1]))
-        handler=handleRequestsUsing(conn)
+        handler = handleRequestsUsing(conn)
         myServer = HTTPServer(('', self.http_port), handler)
         # start new thread takes 1st argument as a function name to be run,
         # second is the tuple of arguments to the function.
-        #SSHTunnelClient(conn).start()
-        #start_new_thread(clientthread, (conn,))
-        
+        # SSHTunnelClient(conn).start()
+        # start_new_thread(clientthread, (conn,))
+
         try:
             myServer.serve_forever()
         except KeyboardInterrupt:
             pass
 
         myServer.server_close()
-        
+
         sock.close()
-            
+
+
 def handleRequestsUsing(ssh_socket):
-      return lambda *args: MyServer(ssh_socket, *args)
+    return lambda *args: MyServer(ssh_socket, *args)
 
 if __name__ == "__main__":
     # host http_port ssh_port
