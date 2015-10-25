e5d9bd7a52469ca94fe83c51497c787232391181
rkouere <rkouere@gmail.com>
2015-10-25 10:07:16 +0100

    ajout user agent + utilisation param pour definir la machine distante

--- bin/entreprise.py
+++ bin/entreprise.py
@@ -10,20 +10,27 @@ from tools import client
 import threading
 import urllib.request
 import base64 as B64
+import sys
 
 #  maison = "http://vps205524.ovh.net/"
 # maison = "http://5.196.70.218"
-maison = "http://localhost"
+# maison = "http://localhost"
+
+user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/"
+user_agent += "537.36 (KHTML, like Gecko) Chrome/45.0.2454.101 Safari/537.36"
 
 
 def log(message):
     print("[maison] " + message)
 
 
-def sendSSHRequestToMaison(data):
+def sendSSHRequestToMaison(data, maison):
     try:
         log("sending POST")
-        req = urllib.request.Request(maison, B64.b64encode(data))
+        data_b64 = B64.b64encode(data)
+        req = urllib.request.Request(maison, data_b64)
+        req.add_header("User-Agent", user_agent)
+        req.add_header("Content-length", len(data_b64))
 
         html = urllib.request.urlopen(req).read()
         print(html)
@@ -32,9 +39,15 @@ def sendSSHRequestToMaison(data):
         return False
 
 
-def getSSHRequestFromMaison():
+def getSSHRequestFromMaison(maison):
+    """
+    Sends GET requests to maison
+    Maison returns a html message is there is no ssh to deal with
+    Maison returs a ssh base 64 message if there is something ssh-y to do
+    """
     try:
         req = urllib.request.Request(maison)
+        req.add_header("User-Agent", user_agent)
         data = urllib.request.urlopen(req).read()
         print(str(data))
         if(len(data) < 1):
@@ -56,9 +69,10 @@ def sendToSSH(ssh_con, req):
 
 
 class sendFromSSH(threading.Thread):
-    def __init__(self, ssh_con):
+    def __init__(self, ssh_con, maison):
         threading.Thread.__init__(self)
         self.ssh_con = ssh_con
+        self.maison = maison
         print("thread started")
 
     def run(self):
@@ -67,14 +81,14 @@ class sendFromSSH(threading.Thread):
             if data:
                 print("[ssh] data received = " + str(data))
                 print("[POST] sending the data")
-                sendSSHRequestToMaison(data)
+                sendSSHRequestToMaison(data, self.maison)
                 time.sleep(0.1)
             else:
                 break
         self.ssh.con.close()
 
 
-def init():
+def init(maison):
     """
     Sends packets to maison all the time
     When a packet has arrived
@@ -87,19 +101,19 @@ def init():
     req = False
     while req is False or req is None:
         log("sending first request")
-        req = getSSHRequestFromMaison()
+        req = getSSHRequestFromMaison(maison)
         time.sleep(1)
 
     #  initialise connection to ssh
     ssh_con = client("localhost", 22).initConnection()
     log("connected to ssh")
     #  we start a new thread to deal with the ssh connection
-    sendFromSSH(ssh_con).start()
+    sendFromSSH(ssh_con, maison).start()
     #  we send the request to ssh
     sendToSSH(ssh_con, req)
     #  we loop with a GET request
     while True:
-        data = getSSHRequestFromMaison()
+        data = getSSHRequestFromMaison(maison)
         if data is not False:
             sendToSSH(ssh_con, data)
 
@@ -111,4 +125,4 @@ def init():
 #  standalone, and we run the main() function.
 if __name__ == "__main__":
     #  tunnel(sys.argv[1], int(sys.argv[2]), int(sys.argv[3])).init()
-    init()
+    init("http://" + sys.argv[1])
