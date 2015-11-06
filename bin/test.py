5a3b13b95ae130e85862881479462b49b385e7b0
rkouere <rkouere@gmail.com>
2015-11-06 11:45:44 +0100

    rm unused files

--- bin/test.py
+++
@@ -1,8 +0,0 @@
-import urllib.request
-
-proxy_support = urllib.request.ProxyHandler({"http":"http://proxy.univ-lille1.fr:3128"})
-opener = urllib.request.build_opener(proxy_support)
-urllib.request.install_opener(opener)
-
-html = urllib.request.urlopen("http://vps205524.ovh.net/").read()
-print(html)
